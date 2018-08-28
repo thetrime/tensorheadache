8f33fab330c32a14ae8852523217c5691799d7e8
Matt Lilley <thetrime@gmail.com>
2018-08-28 14:41:28 +0100

    Closer

--- z.py
+++ z.py
@@ -6,74 +6,17 @@ import scipy.fftpack as fft
 import scipy
 from librosa import util
 
+
 samples, rate = librosa.load("testing/negative-00.wav", sr=16000)
-spec = librosa.core.stft(samples, n_fft=2048, hop_length=512, window='hann')
 s1 = librosa.core.stft(samples, n_fft=2048, hop_length=512, window='hann', center=False)
+my_spectrogram = np.abs(librosa.core.stft(samples, n_fft=2048, hop_length=512))**2
+their_spectrogram, _ = librosa.core.spectrum._spectrogram(y=samples, S=None, n_fft=2048, hop_length=512, power=2)
 
-def stftz(y, n_fft=2048, hop_length=None, win_length=None, window='hann', center=True, dtype=np.complex64, pad_mode='reflect'):
+# Ok, so far we agree. My spectrogram and their output of _spectrogram are the same.
+mel_basis = librosa.filters.mel(sr=16000, n_fft=2048)
+my_melspectrogram = np.dot(mel_basis, my_spectrogram)
+their_melspectrogram = librosa.feature.melspectrogram(samples, sr=16000)
 
-    # By default, use the entire frame
-    if win_length is None:
-        win_length = n_fft
-
-    # Set the default hop, if it's not already specified
-    if hop_length is None:
-        hop_length = int(win_length // 4)
-
-    fft_window = scipy.signal.get_window(window, win_length, fftbins=True)
-
-    # Pad the window out to n_fft size
-    fft_window = util.pad_center(fft_window, n_fft)
-
-    # Reshape so that the window can be broadcast
-    fft_window = fft_window.reshape((-1, 1))
-
-    # Check audio is valid
-    util.valid_audio(y)
-
-    # Pad the time series so that frames are centered
-    if center:
-        y = np.pad(y, int(n_fft // 2), mode=pad_mode)
-
-    # Window the time series.
-    y_frames = util.frame(y, frame_length=n_fft, hop_length=hop_length)
-
-    # Pre-allocate the STFT matrix
-    stft_matrix = np.empty((int(1 + n_fft // 2), y_frames.shape[1]),
-                           dtype=dtype,
-                           order='F')
-
-    # how many columns can we fit within MAX_MEM_BLOCK?
-    n_columns = int(util.MAX_MEM_BLOCK / (stft_matrix.shape[0] *
-                                          stft_matrix.itemsize))
-    for bl_s in range(0, stft_matrix.shape[1], n_columns):
-        bl_t = min(bl_s + n_columns, stft_matrix.shape[1])
-
-        # RFFT and Conjugate here to match phase from DPWE code
-        stft_matrix[:, bl_s:bl_t] = fft.fft(fft_window *
-                                            y_frames[:, bl_s:bl_t],
-                                            axis=0)[:stft_matrix.shape[0]]
-
-    return stft_matrix
-
-s2 = stftz(samples, n_fft=2048, hop_length=512, window='hann', center=False)
-
-
-"""
-Data; 6.048683e-02i + 0.000000e+00j
-Data; -3.145323e-02i + 4.041574e-01j
-Data; -3.935143e-01i + -6.830263e-01j
-Data; 4.084088e-01i + 7.473204e-01j
-
-
-
->>> s2[0][0]
-(0.06048683+0j)
->>> s2[1][0]
-(-0.031453233+0.4041574j)
->>> s2[2][0]
-(-0.39351434-0.68302625j)
->>> s2[3][0]
-(0.40840876+0.74732035j)
->>> s2[4][0]
-(0.17209564-0.5555018j)
+S = librosa.core.spectrum.power_to_db(my_melspectrogram)
+my_mfcc = scipy.fftpack.dct(S, axis=0, type=2, norm='ortho')[:26]
+their_mfcc = librosa.feature.mfcc(y=samples, sr=16000, n_mfcc=26)
